a03be457300e0cc30e0b051f187cb55164e4ec4c
Sialovesanimals123 <162298429+Sialovesanimals123@users.noreply.github.com>
2025-06-06 18:25:35 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,3 +9,20 @@ function page2(){
 function page3(){
     window.open("hs.html")
 }
+
+function createsnowflake(){
+    var snowflake = document.createElement('div')
+    snowflake.classList.add('snowflake')
+    snowflake.textContent='❄️'
+    snowflake.style.left= Math.random() * window.innerWidth + 'px';
+    var falltime = 5 + Math.random() * 5;
+    snowflake.style.animationDuration= falltime + 's'
+    document.body.appendChild(snowflake);
+
+    setTimeout(()=>{
+        snowflake.remove();
+    }, falltime*2000)
+    
+}
+
+setInterval(createsnowflake, 500)
